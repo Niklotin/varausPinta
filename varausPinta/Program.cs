6803cea42b7295756558639bf69795372a53f9fc
Niko Korajoki <korajoki430@live.fi>
2026-01-29 11:16:23 +0200

    Tekoäly käytti AddScoped funktiota jossa palvelun data nollautuu jokaisen HTTP-pyynnön jälkeen.
    Me käytetään muistinvaraista tietokantaa niin tarvitaan Singleton rekisteröinti ettei data lehahda karkuun tehdään uusia pyyntöjä.

--- varausPinta/Program.cs
+++ varausPinta/Program.cs
@@ -2,23 +2,14 @@ using varausPinta.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddScoped<BookingService>();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSingleton<BookingService>();
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
